d0b938b68db3a4187691b056271d3d718c8b109d
Daniel Lee <dlee13195@gmail.com>
2023-02-21 19:02:28 -0500

    checking render

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -19,8 +19,6 @@ class Reservation < ApplicationRecord
 
     # validate :end_date_after_start_date
 
-
-    
     belongs_to :user,
     class_name: :User,
     foreign_key: :user_id
@@ -29,22 +27,6 @@ class Reservation < ApplicationRecord
     class_name: :Listing,
     foreign_key: :listing_id
 
-    # private
 
-    # def end_date_after_start_date
-    #     if end_date && start_date && end_date <= start_date
-    #       errors.add(:end_date, "must be after the start date")
-    #     end
-    # end
-
-    def valid_date
-        if start_date > end_date
-            errors.add(:start_date, 'The start of a reservation must be before the end of a reservation')
-        end
-
-        if (start_date < Date.today) || (end_date < Date.today)
-            errors.add(:start_date, 'Reservations cannot be in the past')
-        end
-    end
 
 end
