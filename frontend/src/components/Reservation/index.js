44c097b62e6f9764df1d2470bbf194d58f7adfc3
Daniel Lee <dlee13195@gmail.com>
2023-03-12 22:15:02 -0400

    css for reservations index

--- frontend/src/components/Reservation/index.js
+++ frontend/src/components/Reservation/index.js
@@ -29,28 +29,26 @@ function ReservationIndex() {
   };
 
   return (
-    <div>
+    <div className="Trips-index">
       <div className="trips-header">Trips</div>
       {userReservations.length === 0 && (
         <div className="no-trips">
-          <div>No trips booked...yet!</div>
-          <div>Time to dust off your bags and start planning your next adventure</div>
+          <h2 className="no-trips-text">No trips booked...yet!</h2>
+          <div className="no-trips-text2">Time to dust off your bags and start planning your next adventure</div>
           <NavLink exact to="/" className="index-navlink" style={{ textDecoration: "none" }}>Start searching</NavLink>
         </div>
       )}
       {userReservations.map((reservation) => (
         <div key={reservation.id} className="reservations-index">
           <div>
-            Start date: {moment(reservation.startDate).format("MM/DD/YYYY")}
-          </div>
-          <div>
-            End date: {moment(reservation.endDate).format("MM/DD/YYYY")}
+            {moment(reservation.startDate).format("MM/DD/YYYY")} - {moment(reservation.endDate).format("MM/DD/YYYY")}
           </div>
           <div>Number of Guests: {reservation.numGuests}</div>
-          <button onClick={() => handleDeleteReservation(reservation.id)}>
+          <button onClick={() => handleDeleteReservation(reservation.id)} className="cancel-res">
             Cancel Reservation
           </button>
         </div>
+        
       ))}
     </div>
   );
