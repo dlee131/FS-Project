3220f4276fec12d9df8dbf80c557b66d273403fa
Daniel Lee <dlee13195@gmail.com>
2023-03-25 17:02:22 -0400

    create and update action controller for reviews

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -9,18 +9,35 @@ class Api::ReviewsController < ApplicationController
     end
 
     def show
-
+        @review = Review.find_by(id: params[:id])
+        render: show
     end
 
     def create
-
+        @review = Review.new(review_params)
+        if @review.save 
+            render :show 
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
     end
 
     def update
-        
+        @review = Review.find_by(id: params[:id])
+        if @review.update(review_params)
+            render :show
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
     end
 
     def destroy
+        
+    end
 
+    private
+
+    def review_params
+        params.require(:review).permit(:user_id, :listing_id, :cleanliness, :accuracy, :communication, :location, :check_in, :value)
     end
 end
