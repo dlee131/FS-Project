f8f2d3f676070e21675af9be7709152e15d516f0
Daniel Lee <dlee13195@gmail.com>
2023-04-01 20:34:38 -0400

    username for reviews

--- app/views/api/reviews/index.json.jbuilder
+++ app/views/api/reviews/index.json.jbuilder
@@ -2,5 +2,6 @@
     json.set! review.id do
         json.extract! review,
         :id, :user_id, :listing_id, :cleanliness, :accuracy, :communication, :location, :check_in, :value, :comment
+        json.reviewerName review.user.username
     end
 end
--- frontend/src/components/Review/index.js
+++ frontend/src/components/Review/index.js
@@ -4,7 +4,6 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams, NavLink } from "react-router-dom";
 
 
-
 function ReviewsIndex() {
   const [cleanliness, setCleanliness] = useState(0);
   const [accuracy, setAccuracy] = useState(0);
@@ -17,20 +16,39 @@ function ReviewsIndex() {
   const dispatch = useDispatch();
   const { listingId } = useParams();
   const listing = useSelector((state) => state.listings[listingId]);
+  const { reviewId } = useParams();
+  const review = useSelector((state) => state.reviews[reviewId]);
   const totalReviews = reviews.length;
+  const userId = useParams();
+
 
   useEffect(() => {
     dispatch(fetchReviews(listingId));
   }, [listingId, dispatch]);
 
+//  const EachReview = reviews.forEach(review => {
+//         review.comment
+//   })
+
+const eachReview = reviews.map(review => {
+    // Access each review using the `review` variable
+    return (
+      <div key={review.id}>
+        <p>{review.comment}</p>
+        <p>{review.reviewerName}</p>
+      </div>
+    );
+  });
+  
   return (
     <div className="reviews-index">
     <div className="total-reviews">
-      <i className="fas fa-star"></i>{totalReviews} {totalReviews === 1 ? "reviews" : "reviews"}
+      <i className="fas fa-star"></i>{totalReviews} {totalReviews === 1 ? "review" : "reviews"}
     </div>
     <div>
         <NavLink exact to={`/listings/${listingId}/reviews/new`}>Write a review!</NavLink>
     </div>
+        {eachReview}
     </div>
   );
 }
