8c5fdd1cb33f0cdd3d49979ef9b1ef712f392816
Daniel Lee <dlee13195@gmail.com>
2023-04-17 20:20:59 -0400

    map toggle css

--- frontend/src/components/ListingsIndex/index.js
+++ frontend/src/components/ListingsIndex/index.js
@@ -1,34 +1,33 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux"
-import { fetchListings, getListings } from "../../store/listings"
-import  ListingsItem  from './ListingsItem';
-import { useHistory } from "react-router-dom"
-import { Filter } from '../Filters/filter'
+import { useDispatch, useSelector } from "react-redux";
+import { fetchListings, getListings } from "../../store/listings";
+import ListingsItem from "./ListingsItem";
+import { useHistory } from "react-router-dom";
+import { Filter } from "../Filters/filter";
 import ListingMapWrapper from "../ListingMap";
 
-
 function ListingsIndexPage() {
-    const dispatch = useDispatch();
-    const listings = useSelector(getListings)
-    const history = useHistory();
-    const [mapToggle, setMapToggle] = useState(false)
+  const dispatch = useDispatch();
+  const listings = useSelector(getListings);
+  const history = useHistory();
+  const [mapToggle, setMapToggle] = useState(false);
 
-    useEffect(() => {
-        dispatch(fetchListings())
-      },[dispatch])
-      
-    const handleClick = (listingId) => {
-      history.push(`/listings/${listingId}`);
-      window.scrollTo(0, 0);
-    };
+  useEffect(() => {
+    dispatch(fetchListings());
+  }, [dispatch]);
+
+  const handleClick = (listingId) => {
+    history.push(`/listings/${listingId}`);
+    window.scrollTo(0, 0);
+  };
   //map toggle
   const showMap = () => {
-      if (mapToggle === true) {
-          setMapToggle(false)
-      } else {
-          setMapToggle(true)
-      }
-  }
+    if (mapToggle === true) {
+      setMapToggle(false);
+    } else {
+      setMapToggle(true);
+    }
+  };
 
   return (
     <div>
@@ -56,31 +55,30 @@ function ListingsIndexPage() {
                 center: { lat: 40.754787, lng: -73.820393 },
                 zoom: 8,
               }}
-            >
-            </ListingMapWrapper>
+              className="map-index"
+            ></ListingMapWrapper>
           </div>
         )}
         <div className="index-map-container">
-        <div className="toggle-button">
-              <button onClick={showMap}>
-                {!mapToggle ? (
-                  <p>
-                    Show map<i className="fa-solid fa-list-ul fa-show"></i>
-                  </p>
-                ) : (
-                  <p>
-                    Show list<i className="fa-solid fa-map fa-show"></i>
-                  </p>
-                )}
-              </button>
-              </div>
+          <div className="toggle-button">
+            <button onClick={showMap}>
+              {!mapToggle ? (
+                <div className="map-text">
+                  Show map<i className="fa-solid fa-map fa-show"></i>
+                </div>
+              ) : (
+                <div className="map-text">
+                  Show list<i className="fa-solid fa-list-ul fa-show"></i>
+                </div>
+              )}
+            </button>
+          </div>
         </div>
       </div>
       <div className="border-line"></div>
       <div className="footer"></div>
     </div>
   );
-};
-
+}
 
 export default ListingsIndexPage;
