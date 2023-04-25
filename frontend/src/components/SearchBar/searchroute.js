ebf80adeccab5d5807f60c8b55410d0d5a66dedd
Daniel Lee <dlee13195@gmail.com>
2023-04-25 19:36:08 -0400

    search route function

--- frontend/src/components/SearchBar/searchroute.js
+++ frontend/src/components/SearchBar/searchroute.js
@@ -1,13 +1,19 @@
 import ListingMapWrapper from "../ListingMap";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
+import { getListings } from "../../store/listings";
+import { useParams } from "react-router-dom";
+
 
 
 function SearchRoute () {
 
     const [] = useState("")
     const dispatch = useDispatch()
-    
+    const listing = useSelector((state) => state.listings[listingId]);
+    const listings = useSelector(getListings);
+    const { listingId } = useParams();
+
 
     return 
         (
@@ -25,3 +31,5 @@ function SearchRoute () {
         </div>
         )
 }
+
+export default SearchRoute
