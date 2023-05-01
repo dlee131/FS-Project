5604eb0b551ac4f03d588920c93eb1bf03854f09
Daniel Lee <dlee13195@gmail.com>
2023-04-30 21:20:41 -0400

    host changed to first name

--- app/views/api/listings/index.json.jbuilder
+++ app/views/api/listings/index.json.jbuilder
@@ -3,6 +3,6 @@
         json.extract! listing,
         :id, :title, :description, :city, :state, :country, :zip_code, :nightly_price, :num_beds, :num_bedrooms, :num_bathrooms, :pets_allowed, :latitude, :longitude, :ratings, :residence_type, :avg_rating
         json.photo listing.photos.map {|photo| photo.url}
-        json.userName listing.host.username
+        json.firstName listing.host.first_name
     end
 end
--- frontend/src/components/ListingPage/index.js
+++ frontend/src/components/ListingPage/index.js
@@ -63,7 +63,7 @@ export const ListingPage = () => {
       </header>
       <div className="info-container">
         <div className="listing-info">
-          <h1 className="residence-type">{`${listing?.residenceType} hosted by ${listing?.userName}`}</h1>
+          <h1 className="residence-type">{`${listing?.residenceType} hosted by ${listing?.firstName}`}</h1>
           <h2 className="listing-bed">
             {listing?.numBedrooms} bedroom • {listing?.numBeds} beds • {listing?.numBathrooms} baths
           </h2>
