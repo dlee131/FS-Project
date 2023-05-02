6cc772001674cdd16ec756f6fc3bf21814383ea5
Daniel Lee <dlee13195@gmail.com>
2023-05-02 08:40:04 -0400

    jbuilder listing for num_guest

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -26,10 +26,11 @@
 
 class Listing < ApplicationRecord
 
-    validates :host_id, :title, :description, :city, :state, :country, :zip_code, :latitude, :longitude, :ratings, :residence_type, presence: true
+    validates :host_id, :title, :description, :city, :state, :country, :zip_code, :latitude, :longitude, :ratings, :residence_type, :num_guest, presence: true
     validates :nightly_price, inclusion: { in: 10...3000, message: "must be between $10 and $3000" } 
     validates :num_beds, :num_bathrooms, :num_bedrooms, numericality: {in: 0..6}
     validates :pets_allowed, inclusion: { in: [true,false] }
+    
 
     belongs_to :host,
     foreign_key: :host_id,
--- app/views/api/listings/index.json.jbuilder
+++ app/views/api/listings/index.json.jbuilder
@@ -1,7 +1,7 @@
 @listings.each do |listing|
     json.set! listing.id do
         json.extract! listing,
-        :id, :title, :description, :city, :state, :country, :zip_code, :nightly_price, :num_beds, :num_bedrooms, :num_bathrooms, :pets_allowed, :latitude, :longitude, :ratings, :residence_type, :avg_rating
+        :id, :title, :description, :city, :state, :country, :zip_code, :nightly_price, :num_beds, :num_bedrooms, :num_bathrooms, :pets_allowed, :latitude, :longitude, :ratings, :residence_type, :avg_rating, :num_guest
         json.photo listing.photos.map {|photo| photo.url}
         json.firstName listing.host.first_name
     end
