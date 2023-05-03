7975174acf06706fe0d2c813b9f88a44ddb39191
Daniel Lee <dlee13195@gmail.com>
2023-05-03 17:13:52 -0400

    user's first name displayed

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -1,17 +1,16 @@
 import * as sessionActions from "../../store/session";
 import { useSelector } from "react-redux";
 
-function Profile () {
+function Profile() {
+  const sessionUser = useSelector((state) => state.session.user);
+  const user = useSelector((state) => state.session.user);
 
-    const sessionUser = useSelector((state) => state.session.user);
-
-
-    return (
-        <>
-        <div>Hello!</div>
-        </>
-    );
-
+  return (
+    <>
+      <div>Hello {user.first_name}!</div>
+      
+    </>
+  );
 }
 
 export default Profile;
