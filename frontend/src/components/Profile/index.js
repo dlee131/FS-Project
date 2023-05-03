8190a65203a613e33be213395cfeb24a379c5ab7
Daniel Lee <dlee13195@gmail.com>
2023-05-03 17:30:48 -0400

    user profile info

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -1,16 +1,19 @@
 import * as sessionActions from "../../store/session";
 import { useSelector } from "react-redux";
 import { formatDate } from "../Review";
+import "./Profile.css"
 
 function Profile() {
   const sessionUser = useSelector((state) => state.session.user);
   const user = useSelector((state) => state.session.user);
 
   return (
-    <>
-      <div>Hello {user.first_name}!</div>
-      <div>Joined on {formatDate(user.created_at)}</div>
-    </>
+      <div className="profile-container">
+        <div className="user-first-name">Hello {user.first_name}!</div>
+        <div>Welcome to Melobnb!</div>
+        <div>Email: {user.email}</div>
+        <div>Joined on {formatDate(user.created_at)}</div>
+      </div>
   );
 }
 
