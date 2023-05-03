733658c0b602aaecb968410fe773b3b967755453
Daniel Lee <dlee13195@gmail.com>
2023-05-03 16:59:19 -0400

    profile implementation

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -1,10 +1,14 @@
-
+import * as sessionActions from "../../store/session";
+import { useSelector } from "react-redux";
 
 function Profile () {
 
+    const sessionUser = useSelector((state) => state.session.user);
+
+
     return (
         <>
-        <div> Hi </div>
+        <div>Hello!</div>
         </>
     );
 
